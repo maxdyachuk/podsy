0513f585870780717eca74d89d5f325460360f64
Max Dyachuk <51743080+maxdyachuk@users.noreply.github.com>
2020-03-16 11:49:24 +0200

    update log in implementation

--- client/src/app/components/log-in/log-in.component.ts
+++ client/src/app/components/log-in/log-in.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormControl, FormGroup } from '@angular/forms';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-log-in',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LogInComponent implements OnInit {
 
-  constructor() { }
+  loading: boolean;
+
+  loginForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder,
+    private snackBar: MatSnackBar) { }
 
   ngOnInit() {
+    this.loading = false;
+    this.loginForm = this.formBuilder.group({
+      email: [undefined, [Validators.required, Validators.email]],
+      password: [undefined, [Validators.required]],
+    });
+  }
+
+  onSubmit() {
+    this.loading = true;
+  }
+
+  openSnackBar(message: string, action: string) {
+    this.snackBar.open(message, action, {
+      duration: 5000,
+    });
   }
 
 }
